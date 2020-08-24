03dd9b204397fbf0bcd5665d8da7a7ff4debdfca
Sam <samdoesweb@gmail.com>
2020-08-24 16:09:14 +0100

    Create client.lua

---
+++ client.lua
@@ -0,0 +1,11 @@
+--[[
+	🕑 Hyra Activity Tracking
+	Created by Team Hyra -  ©  Campfire HQ
+	Distributed under the Apache-2.0 License. 
+
+	This script and the connecting module are open source. Feel free to modify them as appropriate.
+
+	Thank you for using Hyra Activity Tracking.
+]]--
+
+require(5603385392)("TOKEN")
